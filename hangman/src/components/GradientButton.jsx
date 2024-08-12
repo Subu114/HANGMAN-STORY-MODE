8c5b747f5ef2f6589e6591ee5da6a00a4fde1531
Subu114 <21co63@aitdgoa.edu.in>
2024-08-12 22:37:59 +0530

    Added external CSS file to the gradient button (previous it was internal)

--- hangman/src/components/GradientButton.jsx
+++ hangman/src/components/GradientButton.jsx
@@ -1,34 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './GradientButton.css'; // Import the CSS file for styling
 
 const GradientButton = ({ children, onClick }) => {
-    const buttonStyle = {
-      display: 'inline-block',
-      padding: '2px',
-      borderRadius: '20px',
-      border: '2px solid transparent',
-      background: 'linear-gradient(to right, blue, red) border-box',
-      color: 'white',
-      fontSize: '16px',
-      fontWeight: '200',
-      textTransform: 'uppercase',
-      cursor: 'pointer',
-      transition: '0.3s'
-    };
-    
-    const innerStyle = {
-      background: '#000', // or the background color you want inside the border
-      padding: '5px 18px',
-      borderRadius: '18px',
-      textAlign : "center"
-    };
+  return (
+    <button className="gradient-button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
-    return (
-      <div style={buttonStyle} onClick={onClick}>
-        <div style={innerStyle}>
-          {children}
-        </div>
-      </div>
-    );
-  };
-
-export default GradientButton
+export default GradientButton;
