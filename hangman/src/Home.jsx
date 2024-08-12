53f6d1af0f327dc51762f7c0990f178339fcdbc9
Subu114 <21co63@aitdgoa.edu.in>
2024-08-12 22:36:30 +0530

    Added checking the token validity when user first presses the play button

--- hangman/src/Home.jsx
+++ hangman/src/Home.jsx
@@ -2,9 +2,36 @@ import './Home.css';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import content from './info';
+import { message } from 'antd';
+import axios from 'axios';
+import { serverUrl } from './config/serverUrl';
+import { token } from './config/userData';
 const Home = () => {
   const navigate = useNavigate();
-  
+  const handlePlay = async () =>{
+    try {
+      const res = await axios({
+        method : "POST",
+        url : `${serverUrl}/users/auth`,
+        headers : {
+          Authorization : `Bearer ${token}`
+        }
+      })
+      navigate("/user")
+    } catch (error) {
+      if(error.response.status === 403)
+      {
+        console.log("User not authenticated")
+        message.error("Session expired")
+        navigate("/signin")
+      }
+      else{
+        message.error(error.response ? error.response.message : error.message)
+        console.log("error : ", error.response ? error.response.message : error.message)
+        navigate("/")
+      }
+    }
+  }
   return (
     <>
       <div className="scrollable-container" >
@@ -27,7 +54,7 @@ const Home = () => {
         <div className='space'>
         </div>
         <div className='play-button'>
-          <button onClick={() => navigate("/user")}>PLAY</button>
+          <button onClick={handlePlay}>PLAY</button>
         </div>
       </div>
       <div className='story-container'>
