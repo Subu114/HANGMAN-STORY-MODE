88f00b0ac53945576c5166f785f48fb78d62fb32
Subu114 <21co63@aitdgoa.edu.in>
2024-08-12 15:04:54 +0530

    Fixed a bug

--- hangman/src/hooks/useFetchWord.js
+++ hangman/src/hooks/useFetchWord.js
@@ -2,41 +2,43 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { sceneWordKey } from '../config/sceneWordKey';
 import { token } from '../config/userData';
-
-const serverUrl = import.meta.env.VITE_SERVER_URL;
+import { serverUrl } from '../config/serverUrl';
 
 
 const useFetchWord = (sceneNumber) => {
     const [loading, setLoading] = useState(false)
     const [data, setData] = useState(null)
     const [error, setError] = useState(null)
-    // console.log(serverUrl)
-    const fetchData = async () => {
-        try {
-            setLoading(true)
-            const response = await axios.get(`${serverUrl}/scenes/word/${sceneNumber}`,{
-                params : {unique_token : sceneWordKey},
-                headers : {
-                    'Authorization' : `Bearer ${token}`
-                }
-            })
-            setData(response.data.scene_word.toUpperCase())
-            
-        } 
-        catch (error) {
-            console.log("Error Fetching Scene Word : ", error)
-            setError(error)
-        }
-        finally{
-            setLoading(false)
-        }
-    }
 
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true)
+                console.log(token)
+                const response = await axios({
+                    method: "POST",
+                    url: `${serverUrl}/scenes/word/${sceneNumber}`,
+                    data: { unique_token: sceneWordKey },
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                setData(response.data.scene_word.toUpperCase())
+
+            }
+            catch (error) {
+                console.log("Error Fetching Scene Word : ", error)
+                setError(error)
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+
         fetchData();
     }, [])
 
-    return {data, loading, error}
+    return { data, loading, error }
 }
 
 export default useFetchWord
